68c41dc7477bc4a7ce1c64df424308de5092f539
rizky aziz <rizkyaziz2002@gmail.com>
2022-03-07 14:12:42 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./public/**/*.{html, js}', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        "blue-brand": "#06dbdb",
+      },
+      spacing: {
+        "40px": "40px",
+        "100px": "100px",
+      },
+      minWidth: {
+        "40px": "40px",
+        "100px": "100px",
+      },
+      maxWidth: {
+        "40px": "40px",
+        "100px": "100px",
+      },
+    },
+  },
+  plugins: [],
+}
